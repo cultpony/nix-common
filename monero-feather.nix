411bd720b35bb5bf60792ec90f6b0d1e893c2fae
CULT PONY <67918945+cultpony@users.noreply.github.com>
2023-11-20 15:09:09 +0100

    fixup monero setup

--- monero-feather.nix
+++ monero-feather.nix
@@ -6,16 +6,17 @@
 appimageTools.wrapType2 rec {
   name = "feather";
   version = "2.5.2";
-  src = {
-    x86_64-linux = fetchurl {
+  src = if system == "x86_64-linux" then
+    fetchurl {
       url = "https://featherwallet.org/files/releases/linux-appimage/feather-${version}.AppImage";
       hash = "sha256-P5wg9NE9NC1KcT2JNW0OXKsPxmCZs4+nd2mbzWSRGT0=";
-    };
-    aarch64-linux = fetchurl {
+    }
+  else if system == "aarch64-linux" then
+    fetchurl {
       url = "https://featherwallet.org/files/releases/linux-arm64-appimage/feather-${version}-arm64.AppImage";
       hash = "sha256-r4xVmcQBNlw2ky82xpkhwufoDYDEUgOi9aP9+Ts7dGo=";
-    };
-  }.${system};
+    }
+  else "incompatible system";
   extraPkgs = pkgs: with pkgs; [];
   meta = with lib; {
     description = "Monero Light Wallet for Desktop";
