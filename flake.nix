58a259cb22a97c6e3823a560ee4978f197f7a5b8
cultpony <67918945+cultpony@users.noreply.github.com>
2024-07-09 18:09:43 +0200

    remove dead code

--- flake.nix
+++ flake.nix
@@ -37,40 +37,9 @@
             nixpkgs-fmt
             nixfmt
             python310Packages.nix-prefetch-github
-            (let
-              owner = "glitch-soc";
-              repo = "mastodon";
-              ver = "4.2.3-glitch-patched";
-              rev = "3341db939cd077820ad598b0445d02ab2382eaf4";
-            in writeShellScriptBin "mastodonUpdate.sh" ''
-              set -euo pipefail
-              cd $(${git}/bin/git rev-parse --show-toplevel)/mastodon-pkg
-              export NIXPKGS=${nixpkgs-unstable}
-              ./update.sh --owner "${owner}" --repo "${repo}" --ver "${ver}" --rev "${rev}"
-              # --patches ../patches/0002-yarn-typescript.patch
-              # --patches "../mastodon-pre-cve1.patch ../mastodon-cve.patch"
-            '')
           ];
         };
         packages = {
-          mastodonSrc = with pkgs-unstable;
-            (callPackage ./mastodon-pkg/source.nix { });
-
-          mastodon = with pkgs-unstable;
-            (callPackage ./mastodon-pkg/default.nix { inherit self; });
-
-          mastodonYarnCache = with pkgs-unstable;
-            (callPackage ./mastodon-pkg/yarnOfflineCache.nix {
-              inherit self;
-              hash = "sha256-sPdBzV4vpN7m7yMm4t2/xCXN3rLI7PIRwrEWF26FTAk=";
-            });
-
-          mastodonEmojiImporter = with pkgs-unstable;
-            (callPackage ./mastodon-pkg/mastodonEmojiImporter.nix { });
-
-          mastodonGems = with pkgs-unstable;
-            (callPackage ./mastodon-pkg/bundlerEnv.nix { inherit self; });
-
           monero-feather = with pkgs;
             (qt6.callPackage ./monero-feather.nix { });
 
@@ -155,10 +124,8 @@
 
         checks.test-hydrus = self.packages.${system}.hydrus;
         checks.monero-feather = self.packages.${system}.monero-feather;
-        checks.mastodon = self.packages.${system}.mastodon;
       })) // {
         lib = {
-          mastodonGemSet = import ./mastodon-pkg/gemset.nix;
           cachix = import ./cachix.nix;
           cultpony_ssh_keys = import ./cultpony_ssh_keys.nix;
           common_setup = import ./common_setup.nix;
@@ -167,7 +134,6 @@
           pgbackrest = import ./pgbackrest.nix;
           restic_noprune = import ./restic_noprune.nix;
           rustic = import ./rustic.nix;
-          mastodon = import ./mastodon.nix;
           mixins = {
             nginx = import ./nginx.nix;
             server-common = import ./server-common.nix;
